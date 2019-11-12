88b2d5c6c31ef4d8a6df8f37fee0dc9e387d0d41
Count Saint-Germain <ncolic@gmail.com>
2019-11-12 13:52:31 +0100

    better

--- correlations.py
+++ correlations.py
@@ -2,6 +2,8 @@ import os
 import json
 
 medis = {}
+medis_names = {}
+diag_names = {}
 medis_counts = {}
 error_counter = 0
 
@@ -22,19 +24,28 @@ for file in os.listdir("../18k"):
 					for c in d['CodeList']:
 						document_d_codes.append(c['Code'])
 						
+						if not c['Code'] in diag_names.keys():
+							diag_names[c['Code']] = c['Name']
+						
 			
 			for m in g['ZsfgKG']['MediEintritt']['Medis']['List']:
 				if not m['ATC'] in medis.keys():
 					medis[m['ATC']] = document_d_codes
 				else:
 					medis[m['ATC']].extend(document_d_codes)
+					
+				if not m['ATC'] in medis_names.keys():
+					medis_names[m['ATC']] = m['oWirkstoff']
 		except:
 			# print('Error in ', fname)
 			error_counter += 1
 
-print('Error filtes: ', error_count)		
-del medis['']
+print('Error filtes: ', error_counter)
 
+if '' in medis.keys():	
+	del medis['']
+
+print_dict = {}
 for m , c in medis.items():
 	counts = {}
 	d_codes = set(c)
@@ -45,16 +56,17 @@ for m , c in medis.items():
 		count = c.count(d)
 		
 		if count > 100:
-			counts[d] = c.count(d)
+			counts[diag_names[d]] = c.count(d)
 	
 	if len(counts) > 0:
-		counts_sorted = sorted(counts.items(), key=lambda kv: kv[1]).reverse()
-		medis_counts[m] = counts_sorted
+		counts_sorted = sorted(counts.items(), key=lambda kv: kv[1], reverse=True)
+		
+		print_dict[medis_names[m]] = counts_sorted
 	
 
 		
 with open('out.txt','w') as g:
-	g.write(json.dumps(medis_counts,sort_keys=True,indent=4))
+	g.write(json.dumps(print_dict,sort_keys=True,indent=4))
 				
 	
 		
